2d2a5f0b7109747421f7209a9729ff821681a677
Luiza Pessoa <lpessoa@lenovo.com>
2024-08-14 14:22:31 -0300

    modified:   README.md 	new file:   avarage_performance.asm

---
+++ avarage_performance.asm
@@ -0,0 +1,10 @@
+###########################################################################################
+# O programa abaixo funciona como um classificador de índice de rendimento acadêmico
+# Além disso, esta aplicação trabalha com duas funcionalidades principais: 
+# 1.1.Números inteiros recebidos por input do usuário
+# 1.4.Estrutura condicional composta (if, else if e else)
+# Entrada: três números inteiros
+# Saída: Média aritmética dos valores e situação do usuário (Reprovado, AF, Aprovado)
+###########################################################################################
+
+
